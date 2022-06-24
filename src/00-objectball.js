4af1e0f7ecbac4020a456ce34830bf99cc3a248b
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-06-24 11:34:40 +0300

    commit tree c8eced8cb56a4716b8c98e298c1a8a265a0b2026

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,121 +1,132 @@
-const gameObject={
-    home:{
-        teamName:"Brooklyn Nets",
-        colors: "Black, White",
-        players:{
-            "Alan Anderson":{
-                "number":0,
-                "shoe":16,
-                "points":22,
-                "rebounds":12,
-                "Assists":12,
-                "Steals":3,
-                "Blocks":1,
-                "Slam Dunks":1
-            },
-            "Reggie Evans":{
-                "number":30,
-                "shoe":14,
-                "points":12,
-                "rebounds":12,
-                "Assists":12,
-                "Steals":12,
-                "Blocks":12,
-                "Slam Dunks":7
-            },
-            "Brook Lopez":{
-                "number":11,
-                "shoe":17,
-                "points":17,
-                "rebounds":19,
-                "Assists":10,
-                "Steals":3,
-                "Blocks":1,
-                "Slam Dunks":15
-            },
-            "Mason Plumlee":{
-                "number":1,
-                "shoe":19,
-                "points":26,
-                "rebounds":12,
-                "Assists":6,
-                "Steals":3,
-                "Blocks":8,
-                "Slam Dunks":5
-            },
-            "Jason Terry":{
-                "number":31,
-                "shoe":15,
-                "points":19,
-                "rebounds":2,
-                "Assists":2,
-                "Steals":4,
-                "Blocks":11,
-                "Slam Dunks":1
-            },
-        }
+function gameObject(){
+    let game = {
+        home:{
+            teamName:"Brooklyn Nets",
+            colors: "Black, White",
+            players:{
+                "Alan Anderson":{
+                    "Number": 0,
+                    "shoe":10,
+                    "points":22,
+                    "rebounds":12,
+                    "Assists":12,
+                    "steals":3,
+                    "blocks":1,
+                    "slam Dunks":1
+                },
+                "Reggie Evans":{
+                    "Number": 30,
+                    "shoe":10,
+                    "points":22,
+                    "rebounds":12,
+                    "Assists":12,
+                    "steals":3,
+                    "blocks":1,
+                    "slam Dunks":1
+                },
+                "Brook Lopez":{
+                    "Number": 11,
+                    "shoe":17,
+                    "points":17,
+                    "rebounds":19,
+                    "Assists":10,
+                    "steals":3,
+                    "blocks":1,
+                    "slam Dunks":15
+                },
+                "Mason Plumlee":{
+                    "Number": 0,
+                    "shoe":10,
+                    "points":22,
+                    "rebounds":12,
+                    "Assists":12,
+                    "steals":3,
+                    "blocks":8,
+                    "slam Dunks":5
+                }
+            }
         },
         away:{
-            teamName: "Charlotte Hornets",
-            colors: ["Turquoise", "Purple"],
+            teamName:"Charlotte Hornets",
+            colors: "Turquoise, Purple",
             players:{
                 "Jeff Adrien":{
-                    "number":4,
-                    "shoe":18,
-                    "points":10,
-                    "rebounds":1,
-                    "Assists":1,
-                    "Steals":2,
-                    "Blocks":7,
-                    "Slam Dunks":2
+                    "Number": 0,
+                    "shoe":10,
+                    "points":22,
+                    "rebounds":12,
+                    "Assists":12,
+                    "steals":3,
+                    "blocks":1,
+                    "slam Dunks":1
                 },
-
                 "Bismak Biyombo":{
-                    "number":31,
-                    "shoe":15,
-                    "points":19,
-                    "rebounds":2,
-                    "Assists":2,
-                    "Steals":4,
-                    "Blocks":11,
-                    "Slam Dunks":1
+                    "Number": 30,
+                    "shoe":10,
+                    "points":22,
+                    "rebounds":12,
+                    "Assists":12,
+                    "steals":3,
+                    "blocks":1,
+                    "slam Dunks":1
                 },
-
                 "DeSagna Diop":{
-                    "number":2,
-                    "shoe":14,
-                    "points":24,
-                    "rebounds":12,
-                    "Assists":12,
-                    "Steals":4,
-                    "Blocks":5,
-                    "Slam Dunks":5
+                    "Number": 11,
+                    "shoe":17,
+                    "points":17,
+                    "rebounds":19,
+                    "Assists":10,
+                    "steals":3,
+                    "blocks":1,
+                    "slam Dunks":15
                 },
-
                 "Ben Gordon":{
-                    "number":8,
-                    "shoe":15,
-                    "points":33,
-                    "rebounds":3,
-                    "Assists":2,
-                    "Steals":1,
-                    "Blocks":1,
-                    "Slam Dunks":0
-                },
-
-                "Brendan Haywood":{
-                    "number":33,
-                    "shoe":15,
-                    "points":6,
+                    "Number": 0,
+                    "shoe":10,
+                    "points":22,
                     "rebounds":12,
                     "Assists":12,
-                    "Steals":22,
-                    "Blocks":5,
-                    "Slam Dunks":12
-                },
-
+                    "steals":3,
+                    "blocks":8,
+                    "slam Dunks":5
+                }
             }
-
         }
+    }
+    return game
 }
-console.log(gameObject());
+function homeTeamName() {
+    let object = gameObject()
+    return object['home']['teamName']
+
+  }
+  
+  function numPointsScored(playerName){
+        let game = gameObject()
+        for (let gameKey in game){
+            const teamObj = game[gameKey]
+            const playerObj = teamObj.players
+            for (const playerKey in playerObj) {
+                if (playerKey === playerName) {
+                    return playerObj[playerKey].points
+                    
+                }
+            }
+        }
+  }
+
+  function shoeSize(pName){
+    let game = gameObject()
+    for (let gameKey in game){
+        const teamObj = game[gameKey]
+        const playerObj = teamObj.players
+        for (const playerKey in playerObj) {
+            if (playerKey === pName) {
+                return playerObj[playerKey].shoe
+                
+            }
+        }
+    }
+}
+  console.log(shoeSize("Jeff Adrien"))
+
